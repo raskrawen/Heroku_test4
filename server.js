d5215a7c9d139ac6b5b4d14101f3e206d4037f8c
Rasmus Kragh Wendelbo <>
2024-11-02 12:07:43 +0100

    port update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 // Serverer statiske filer fra mappen "public"
 app.use(express.static(path.join(__dirname, 'public')));
